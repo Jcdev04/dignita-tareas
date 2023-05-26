dc5d1cd23bd507e0cfd5255a7c07e247666c45da
Jesús Castillo Vidal <jcdev2004@gmail.com>
2023-05-26 01:34:47 -0500

    0.0.6 Se agregó la vista de ventas, sin conflictos

--- Dignita/formsVentas/formVentas.Designer.cs
+++ Dignita/formsVentas/formVentas.Designer.cs
@@ -32,6 +32,11 @@
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
@@ -73,11 +78,61 @@
             this.comboBox2.TabIndex = 0;
             this.comboBox2.Text = "--Seleccione--";
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(22, 176);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(178, 31);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "Agregar empresa";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(22, 213);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(178, 31);
+            this.button2.TabIndex = 12;
+            this.button2.Text = "Ver lista de empresas";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(601, 12);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(178, 31);
+            this.button3.TabIndex = 13;
+            this.button3.Text = "Agregar proyecto";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(517, 12);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(78, 31);
+            this.button4.TabIndex = 14;
+            this.button4.Text = "Editar";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(429, 12);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(82, 31);
+            this.button5.TabIndex = 15;
+            this.button5.Text = "Eliminar";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
             // formVentas
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.groupBox1);
             this.Name = "formVentas";
@@ -95,5 +150,10 @@
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
     }
 }
