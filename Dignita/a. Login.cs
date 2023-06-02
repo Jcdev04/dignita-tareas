ce040a5d5fdb34c91907ab7b13eb34ca2f6db6ff
Nic0Su4 <nicolas.suarez.vera@gmail.com>
2023-06-01 20:18:23 -0500

    Se añadió Form de requerimientos y la Clase Placeholder

--- Dignita/a. Login.cs
+++ Dignita/a. Login.cs
@@ -10,6 +10,7 @@ using System.Drawing.Text;
 using System.Drawing;
 using System.IO;
 using Dignita.Gestion_de_proyectos.vistDesarrollador;
+using Dignita.Gestion_de_proyectos.vistScrum;
 
 namespace Dignita
 {
@@ -69,8 +70,8 @@ namespace Dignita
 
         private void btn_iniciarSesion_Click(object sender, EventArgs e)
         {
-            Realizar_Tareas obj = new Realizar_Tareas();
-            obj.Show();
+            Requerimientos requerimientos = new Requerimientos();
+            requerimientos.Show();
         }
     }
 }
